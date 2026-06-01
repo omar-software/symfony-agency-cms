b29f5f2c05d492bdb6ac088838bf884b30be2a96
Omar Mohamad-Ali <oa4ev@hotmail.com>
2026-06-01 21:12:07 +0200

    Add admin project creation

--- src/Controller/AdminProjectController.php
+++ src/Controller/AdminProjectController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -26,5 +29,43 @@ class AdminProjectController extends AbstractController
         return $this->render('admin_project/index.html.twig', [
             'projects' => $projects,
         ]);
+    }
+
+    // Diese Route zeigt das Formular zum Erstellen eines neuen Projekts
+    #[Route('/admin/projects/new', name: 'app_admin_project_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Hier schützen wir die Seite zusätzlich für Admin-Benutzer
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // Wenn das Formular abgeschickt wurde, speichern wir das neue Projekt
+        if ($request->isMethod('POST')) {
+            $project = new Project();
+
+            // Hier lesen wir die Formularwerte aus
+            $project->setTitle($request->request->get('title'));
+            $project->setDescription($request->request->get('description'));
+            $project->setTechnology($request->request->get('technology'));
+
+            // Checkbox: Wenn sie angeklickt ist, kommt ein Wert zurück
+            $isPublished = $request->request->get('isPublished') ? true : false;
+            $project->setIsPublished($isPublished);
+
+            // Bild-Upload machen wir später, deshalb bleibt imageName erstmal leer
+            $project->setImageName(null);
+
+            // Hier setzen wir das Erstellungsdatum
+            $project->setCreatedAt(new \DateTimeImmutable());
+
+            // Hier speichern wir das Projekt in der Datenbank
+            $entityManager->persist($project);
+            $entityManager->flush();
+
+            // Danach gehen wir zurück zur Projektliste
+            return $this->redirectToRoute('app_admin_projects');
+        }
+
+        // Wenn es kein POST ist, zeigen wir nur das Formular
+        return $this->render('admin_project/new.html.twig');
     }
 }
